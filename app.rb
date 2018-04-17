49da639629d2bb942db5805d5b2f995a81af5356
Michael Pope <popemray1986@gmail.com>
2018-04-17 04:25:54 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,11 +3,23 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do
+    "Click Here To List A Puppy"
+    erb :index
+  end
+
+  get '/welcome' do
+    "Click Here To List A Puppy"
     erb :index
   end
 
   get '/new' do
-    erb :index
+    erb :create_puppy
+  end
+
+  post '/' do
+    @name = "#{params[:name]}"
+    @breed = "#{params[:breed]}"
+    @age = "#{params[:age]}"
   end
 
 
